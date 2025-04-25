36ec5cd06be4ecc7375995b9bd3ee81ce66ea089
Semihhwastaken <semih.fener013@gmail.com>
2025-04-25 09:53:14 +0300

    rabbit

--- NotificationAPI/Settings/RabbitMQSettings.cs
+++ NotificationAPI/Settings/RabbitMQSettings.cs
@@ -2,9 +2,9 @@ namespace NotificationAPI.Settings
 {
     public class RabbitMQSettings
     {
-        public string HostName { get; set; } = "amqps://gxbzryiu:ljV6XHPTqib7u7bdHJC8U3WVd9mj1BCh@leopard.lmq.cloudamqp.com/gxbzryiu";
-        public string UserName { get; set; } = "guest";
-        public string Password { get; set; } = "guest";
+        public string HostName { get; set; } = Environment.GetEnvironmentVariable("RabbitMQ__Host") ?? "localhost";
+        public string UserName { get; set; } = Environment.GetEnvironmentVariable("RabbitMQ__Username") ?? "guest";
+        public string Password { get; set; } = Environment.GetEnvironmentVariable("RabbitMQ__Password") ?? "guest";
         public string NotificationQueueName { get; set; } = "notification_queue";
         public int Port { get; set; } = 5672;
         public int BatchSize { get; set; } = 1000;
